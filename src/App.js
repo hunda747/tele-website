44cb603aea9f2c411608af67e1d2f7dd4c4944ed
Hundaol NK <hundaolnk@gmail.com>
2023-03-10 21:33:57 +0300

    worked on footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./component/layout/navbar/navbar";
 
 import Home from "./pages/Home/home";
+import Footer from "./component/layout/footer/footer";
+
+import { ContentfulClient, ContentfulProvider } from "react-contentful";
+
+const contentfulClient = new ContentfulClient({
+  accessToken: "JLKjHBS5RUxZwemf8yR0ktv1V68WaMGGuZ31iWLRoaU",
+  space: "48vuKCHQ3vJMYoHa7cAmsv",
+});
 
 function App() {
   return (
@@ -15,6 +23,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/component/layout/footer/footer.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "./footer.scss";
+
+export default function Footer(params) {
+  const lists = [
+    {
+      name: "ONLINE SERVICES",
+      list: ["telegebeya", "Online Service Request", "Ethio ecare Service"],
+    },
+    {
+      name: "PERSONAL",
+      list: [
+        "Getting Started",
+        "telecloud",
+        "Recurring Mobile Package",
+        "Long Validity Package",
+        "Mobile Package",
+        "Fixed BB Internet",
+        "Airtime Top Up",
+        "Airtime and Package Credit",
+        "VAS",
+        "CRBT",
+        "Phones & Devices",
+        "Women’s device offer",
+      ],
+    },
+    {
+      name: "BUSINESS",
+      list: [
+        "telecloud",
+        "eSIM",
+        "Fixed Broadband",
+        "Business Mobile Packages",
+        "Mobile Packages",
+        "Mobile Group Offering Package",
+        "VPN",
+        "Corporate CRBT",
+        "DNS & Web Hosting",
+        "Mobile Conference Call",
+        "Wavelength Service",
+      ],
+    },
+    {
+      name: "INTERNATIONAL",
+      list: [
+        "Call & SMS",
+        "Roaming Service",
+        "Airtime Top Up",
+        "Data Service",
+        "VAS",
+        "Wholesale Business",
+        "International Visitors Plan",
+      ],
+    },
+    {
+      name: "CUSTOMER SERVICE",
+      list: [
+        "Ashamtele",
+        "Shop Locations",
+        "Bill Payment Options",
+        "Bill Compliant",
+        "Ethio telecom App Store",
+        "ISupplier Portal",
+        "Ethio telecom – Local Speed Test",
+        "International – Ookla Speed Test",
+        "Contact Us",
+        "FAQ",
+      ],
+    },
+    {
+      name: "COMPANY",
+      list: [
+        "Profile",
+        "History",
+        "Gallery",
+        "CSR",
+        "Fraud Alerts",
+        "Supplier Portal",
+        "Partner's Portal",
+        "Vacancy",
+        "Tender",
+        "Press Release",
+      ],
+    },
+  ];
+
+  return (
+    <div className="footer">
+      <div className="wrapper">
+        <div className="top">
+          {lists.map((list, index) => {
+            return (
+              <div className="section">
+                <h3>{list.name}</h3>
+                {list.list.map((li, index) => {
+                  return <li key={index}>{li}</li>;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
